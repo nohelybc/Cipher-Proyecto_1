8287f6168220018d3fc955253ad057f83c00cf68
Nohely Badillo <nohely.badillo96@gmail.com>
2021-07-03 12:33:44 -0500

    arreglo abecedario

--- src/cipher.js
+++ src/cipher.js
@@ -5,4 +5,12 @@
   
   export default cipher; */
 
-'A'.charCodeAt(0)
+const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+
+
+const cipher = {
+    // ...
+};
+
+export default cipher;
