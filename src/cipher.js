069d7fa1fc3f6bf183d5fef112dc2b793928bb8a
Nohely Badillo <nohely.badillo96@gmail.com>
2021-07-05 13:30:36 -0500

    cambios en funcion de cifrado

--- src/cipher.js
+++ src/cipher.js
@@ -1,16 +1,24 @@
-//esta es la formula base para el cifrado Cesar " (x+n)%26"
-/* const cipher = {
-    // ...
-  };
-  
-  export default cipher; */
-
-const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+//const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
 
 
-const cipher = {
-    // ...
-};
+let desplazamiento = 4;
+let mensaje = "MI MENSAJE SECRETO";
 
-export default cipher;
+mensaje = mensaje.trim();
+
+
+//se define la función para nuestro cipher
+function encode(string, offset) {
+    let valorAscii = mensaje.charCodeAt(i);
+
+    //como las mayusculas se encuentran en un rango de 65 y 90 en valores ascii entonces para nuestra formula de cifrado
+    if (valorAscii >= 65 && <= 90) {
+        desplazo = (valorAscii - 65 + 3) % 26 + 65;
+    }
+}
+
+
+export function encode(cipher) {
+    console.log
+}
