1b43d5b52f181667cf73a44f94dcd0311be56a49
Nohely Badillo <nohely.badillo96@gmail.com>
2021-07-06 12:55:29 -0500

    cambios de estilo en fuentes

--- src/cipher.js
+++ src/cipher.js
@@ -1,22 +1,25 @@
 //const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
+const cipher {
+
+    //let desplazamiento = 4;
+    //let mensaje = "MI MENSAJE SECRETO";
+
+    mensaje = mensaje.trim();
 
 
-let desplazamiento = 4;
-let mensaje = "MI MENSAJE SECRETO";
+    //se define la función para nuestro cipher
+    function encode(string, offset) {
+        let valorAscii = mensaje.charCodeAt(i);
 
-mensaje = mensaje.trim();
-
-
-//se define la función para nuestro cipher
-function encode(string, offset) {
-    let valorAscii = mensaje.charCodeAt(i);
-
-    //como las mayusculas se encuentran en un rango de 65 y 90 en valores ascii entonces para nuestra formula de cifrado
-    if (valorAscii >= 65 && <= 90) {
-        desplazo = (valorAscii - 65 + 3) % 26 + 65;
+        //como las mayusculas se encuentran en un rango de 65 y 90 en valores ascii entonces para nuestra formula de cifrado
+        if (valorAscii >= 65 && <= 90) {
+            desplazo = (valorAscii - 65 + 3) % 26 + 65;
+        }
     }
 }
+
+
 
 
 //export function encode(cipher) {
