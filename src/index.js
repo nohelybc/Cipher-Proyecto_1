62bb21ecac70c2ca2379226918c977fef7552b14
Nohely Badillo <nohely.badillo96@gmail.com>
2021-07-13 12:49:00 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 import cipher from './cipher.js';
 
-
-document.getElementById("cifrar").addEventListener("click", (event) => {
-    event.preventDefault();
-    //se declaran las variables 'globales' que en este caso serían el numero de desplazamiento  y el mensaje a encriptar
-    let string = document.getElementById("string").value;
-
-    let offset = document.getElementById("offset").value;
-    offset = parseInt(offset); //no olvidar usar el ParseInt que es lo que nos permite tomar el vaslor numerico
-
-    let resultado = cipher.encode(offset, string);
-    let mostrarResultado = document.getElementById("string")
-    mostrarResultado.value = resultado;
-})
+//se declaran las variables 'globales' que en este caso serían el numero de desplazamiento  y el mensaje a encriptar
+let string = document.getElementById("string").value;
+let offset = document.getElementById("offset").value;
 
 console.log(string);
 console.log(offset);
 
-console.log(cipher);
+
+document.getElementById("cifrar").addEventListener("click", (event) => {
+    event.preventDefault();
+    //se declaran las variables 'globales' que en este caso serían el numero de desplazamiento  y el mensaje a encriptar
+    offset = parseInt(offset); //no olvidar usar el ParseInt que es lo que nos permite tomar el vaslor numerico
+
+    let resultado = cipher.encode(offset, string);
+    let visualizarResultado = document.getElementById("output");
+    visualizarResultado.value = resultado;
+})
+
+console.log(resultado);
